2a977d63b438be238bb34fddc67004ac1ab345b8
sondos alansi <sondos.alansi@yahoo.com>
2018-08-28 21:10:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+  let hq=42;
+   let distance;
+  if(hq<location){
+  distance=location-hq;
+  }
+  else{
+    distance=hq-location;
+  }
+  return distance;
+}
+console.log(distanceFromHqInBlocks(43));
+console.log(distanceFromHqInBlocks(50));
+console.log(distanceFromHqInBlocks(34));
+
+
+
+function distanceFromHqInFeet(location){
+  distance=distanceFromHqInBlocks(location)*264;
+  return distance;
+}
+console.log(distanceFromHqInFeet(43));
+console.log(distanceFromHqInFeet(50));
+console.log(distanceFromHqInFeet(34));
+
+function 
